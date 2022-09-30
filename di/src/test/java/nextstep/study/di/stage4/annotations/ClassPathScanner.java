9ec37b1a8cb7223cd64bf75470639db96d3be280
Jeong Jinwoo <jwjeong96@gmail.com>
2022-09-30 14:12:55 +0900

    feat: di 코드 solution 추가

--- di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
+++ di/src/test/java/nextstep/study/di/stage4/annotations/ClassPathScanner.java
@@ -3,6 +3,7 @@ package nextstep.study.di.stage4.annotations;
 import java.util.HashSet;
 import java.util.Set;
 import org.reflections.Reflections;
+import org.reflections.scanners.Scanners;
 
 public class ClassPathScanner {
 
@@ -13,5 +14,10 @@ public class ClassPathScanner {
         beanClasses.addAll(reflections.getTypesAnnotatedWith(Repository.class));
         beanClasses.addAll(reflections.getTypesAnnotatedWith(Inject.class));
         return beanClasses;
+    }
+
+    public static Set<Class<?>> getAllClassesInPackageSolution(final String packageName) {
+        final var reflections = new Reflections(packageName, Scanners.SubTypes.filterResultsBy(filter -> true));
+        return reflections.getSubTypesOf(Object.class);
     }
 }
